9299b82cb56aec6c496b4981096f9e99faf1dc9d
prollyhollywood <72751673+prollyhollywood@users.noreply.github.com>
2020-10-19 13:55:42 -0400

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,2 @@
-def on_forever():
-    pass
-forever(on_forever)
+while True:
+    light.show_animation(light.rainbowAnimation, 500)
--- main.ts
+++ main.ts
@@ -1,3 +1,3 @@
-forever(function () {
-	
-})
+while (true) {
+    light.showAnimation(light.rainbowAnimation, 500)
+}
